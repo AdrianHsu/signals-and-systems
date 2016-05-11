2eeeae69a88a24756db82bca0722aef01490f069
AdrianHsu <adrianhsu1995@gmail.com>
2016-05-11 16:21:45 +0800

    2.c

--- fftHan.m
+++ fftHan.m
@@ -16,4 +16,28 @@ for i = 1:N
 end
 
 % (a)
+figure;
 plot(n, w);
+
+% (b)
+
+x(1:N) = 0;
+y(1:N) = 0;
+for i = 1:N
+   x(1, i) = sin(pi * t(1, i)) / (pi * t(1, i));
+   
+   % find(isnan(x)) returns ans = 501
+   % i.e. x(1, 501) = NaN
+   if i == N1 + 1 % i == 501
+       x(1, i) = 1;
+   end
+   y(1, i) = x(1, i) * w(1, i);
+end
+
+figure;
+plot(n, y);
+
+% (c)
+Y = fft(y, N);
+figure;
+plot(n, fftshift(abs(Y)));
